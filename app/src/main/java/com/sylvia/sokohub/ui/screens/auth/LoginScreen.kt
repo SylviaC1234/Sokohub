e6635480ef7c32d6de8a1ebc1b9dc6ebb9579a8a
SylviaC1234 <SylviaC432@gmail.com>
2026-04-07 13:47:44 +0300

    new commit

---
+++ app/src/main/java/com/sylvia/sokohub/ui/screens/auth/LoginScreen.kt
@@ -0,0 +1,157 @@
+package com.sylvia.sokohub.ui.screens.auth
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.sylvia.sokohub.R
+import com.sylvia.sokohub.ui.theme.newBlue
+
+@Composable
+fun LoginScreen(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .paint(painter = painterResource(R.drawable.backgroundimage2), contentScale = ContentScale.FillBounds  ),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+
+
+        )
+    {
+        Image(
+            painter = painterResource(R.drawable.img),
+            contentDescription = "img",
+            modifier = Modifier.size(200.dp),
+
+
+            )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(text = "Welcome back!!",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,)
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        //Variables
+        var email by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+
+
+        //email
+        OutlinedTextField(
+            value = email,
+            onValueChange = {email = it},
+            modifier = Modifier.width(350.dp),
+            leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "") },
+            label = { Text(text = "EmailAddress")},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedBorderColor = newBlue,
+                focusedBorderColor = Color.Black,
+                unfocusedLeadingIconColor = newBlue,
+                unfocusedLabelColor = newBlue,
+
+                )
+
+
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+
+
+        //password
+        OutlinedTextField(
+            value = password,
+            onValueChange = {password = it},
+            modifier = Modifier.width(350.dp),
+            leadingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = "") },
+            label = { Text(text = "password")},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedBorderColor = newBlue,
+                focusedBorderColor = Color.Black,
+                unfocusedLeadingIconColor = newBlue,
+                unfocusedLabelColor = newBlue,
+
+                ),
+            visualTransformation = PasswordVisualTransformation()
+
+
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(
+            onClick = {},
+            colors = ButtonDefaults.buttonColors(newBlue),
+            shape = RoundedCornerShape(10.dp),
+
+            ) {
+            Text(text = "Log in")
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+
+        Text(text = "Don't have an account? Register",
+            fontSize = 15.sp,
+            color = newBlue
+
+        )
+
+
+
+
+
+
+
+
+    }
+
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview(){
+    LoginScreen()
+}
--- app/src/main/java/com/sylvia/sokohub/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/sylvia/sokohub/ui/screens/home/HomeScreen.kt
@@ -1,22 +1,48 @@
 package com.sylvia.sokohub.ui.screens.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.sylvia.sokohub.R
 import com.sylvia.sokohub.ui.theme.newBlue
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -27,6 +53,7 @@ fun HomeScreen(){
 
     )
     {
+        //TopAppBar
         TopAppBar(
             title = {
                 Text(text = "Home")},
@@ -49,19 +76,188 @@ fun HomeScreen(){
                 colors = TopAppBarDefaults.topAppBarColors(
                 containerColor = newBlue,
                 navigationIconContentColor = Color.White,
-                titleContentColor = Color.White
+                titleContentColor = Color.White,
+                actionIconContentColor = Color.White
 
             )
 
 
 
         )
+        //End of TopAppBar
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        //Searchbar
+        var Search by remember { mutableStateOf("") }
+
+        OutlinedTextField(
+            value = Search,
+            onValueChange = { Search = it},
+            modifier = Modifier.fillMaxWidth().padding(10.dp, end = 10.dp),
+            leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = "String")},
+            placeholder = { Text(text = "Search products/Category...")}
+                 )
+
+        //End of Searchbar
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Text(
+            text = "Featured Products",
+            fontSize = 20.sp,
+            color = newBlue,
+            fontWeight = FontWeight.ExtraBold,
+            modifier = Modifier.padding(start = 20.dp)
+
+            )
+
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        //Row
+
+        Row(
+            modifier = Modifier.horizontalScroll(rememberScrollState())
+        ) {
+            Spacer(modifier = Modifier.width(5.dp))
+            //column1
+            Column() {
+
+                Image(
+                    painter = painterResource(R.drawable.grocery1),
+                    contentDescription = "img",
+                    modifier = Modifier.size(200.dp).clip(shape = RoundedCornerShape(20.dp)),
+                    contentScale = ContentScale.Crop,
+
+                )
+
+                Text(text = "Groceries",
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.ExtraBold,
+                    modifier = Modifier.padding(start = 20.dp)
+                )
+                Button(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(newBlue),
+                    shape = RoundedCornerShape(10.dp),
+                    modifier = Modifier.padding(start = 20.dp)
+                ) {
+                    Text(text = "Get Started")
+
+                }
+
+            }
+            //End of column1
+
+            Spacer(modifier = Modifier.width(10.dp))
 
 
 
+            //column2
+            Column() {
+
+                Image(
+                    painter = painterResource(R.drawable.makeup),
+                    contentDescription = "img",
+                    modifier = Modifier.size(200.dp).clip(shape = RoundedCornerShape(20.dp)),
+                    contentScale = ContentScale.Crop,
+
+                    )
+
+                Text(text = "Makeup",
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.ExtraBold,
+                    modifier = Modifier.padding(start = 20.dp)
+                )
+                Button(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(newBlue),
+                    shape = RoundedCornerShape(10.dp),
+                    modifier = Modifier.padding(start = 20.dp)
+                ) {
+                    Text(text = "Get Started")
+
+                }
+
+            }
+            //End of column2
+
+            Spacer(modifier = Modifier.width(10.dp))
+
+
+            //Column3
+            Column() {
+
+                Image(
+                    painter = painterResource(R.drawable.gymequipment),
+                    contentDescription = "img",
+                    modifier = Modifier.size(200.dp).clip(shape = RoundedCornerShape(20.dp)),
+                    contentScale = ContentScale.Crop,
+
+                    )
+
+                Text(text = "Gym Equipment",
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.ExtraBold,
+                    modifier = Modifier.padding(start = 20.dp)
+                )
+                Button(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(newBlue),
+                    shape = RoundedCornerShape(10.dp),
+                    modifier = Modifier.padding(start = 20.dp)
+                ) {
+                    Text(text = "Get Started")
+
+                }
+
+            }
+            //End of column3
+
+            Spacer(modifier = Modifier.width(10.dp))
+
+
+            //Column4
+            Column() {
+
+                Image(
+                    painter = painterResource(R.drawable.toys),
+                    contentDescription = "img",
+                    modifier = Modifier.size(200.dp).clip(shape = RoundedCornerShape(20.dp)),
+                    contentScale = ContentScale.Crop,
+
+                    )
+
+                Text(text = "Toys",
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.ExtraBold,
+                    modifier = Modifier.padding(start = 20.dp)
+                )
+                Button(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(newBlue),
+                    shape = RoundedCornerShape(10.dp),
+                    modifier = Modifier.padding(start = 20.dp)
+                ) {
+                    Text(text = "Get Started")
+
+                }
+
+            }
+            //End of column4
+
+
+        }
+        //End of Row
 
 
     }
+
+
+
+
+
 
 
 
